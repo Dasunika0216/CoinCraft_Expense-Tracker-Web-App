3054c7737e89d6a0454776b86a1d180992ec4348
Dasunika0216 <dasunikamd.22@uom.lk>
2025-03-20 23:21:33 +0530

    Add Dashboard route to App component and enhance Login page with state management and form fields - Frontend part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Footer from './components/Footer'
 import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path = '/' element = {<Home />} />
         <Route path = '/login' element = {<Login />} />
+        <Route path = '/dashboard' element = {<Dashboard />} />
       </Routes>
       <Footer />
     </div>
